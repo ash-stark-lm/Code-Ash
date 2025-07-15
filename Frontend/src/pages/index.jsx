201dc06a7928e18378ec4d74248fb97f40ab1acf
Ashish Kumar <ashish522002@gmail.com>
2025-07-15 11:54:29 +0530

    removed Prob

--- Frontend/src/pages/index.jsx
+++ Frontend/src/pages/index.jsx
@@ -4,6 +4,5 @@ import SignUp from './SignUp'
 
 import Problems from './Problems'
 import ProblemPage from './ProblemPage'
-import Prob from './Prob'
 
-export { HomePage, Login, SignUp, Problems, ProblemPage, Prob }
+export { HomePage, Login, SignUp, Problems, ProblemPage }
