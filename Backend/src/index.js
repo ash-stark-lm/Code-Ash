71ee6f3e9cf9eeba7e2bea3f82b3dbf0db495f74
Ashish Kumar <ashish522002@gmail.com>
2025-07-18 20:46:46 +0530

    fix cors error

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN_TEST,
+    origin: process.env.CORS_ORIGIN,
     credentials: true, // allow cookies / headers
   })
 )
