eb26977589c67d940b951d6b7bedbc3dcc037a00
Ashish Kumar <ashish522002@gmail.com>
2025-07-31 12:38:48 +0530

    fixed splash cursor

--- Frontend/src/components/SplashCursor.jsx
+++ Frontend/src/components/SplashCursor.jsx
@@ -1,20 +1,19 @@
 'use client'
 import { useEffect, useRef } from 'react'
 
-function SplashCursor({
-  // Add whatever props you like for customization
+export default function SplashCursor({
   SIM_RESOLUTION = 128,
   DYE_RESOLUTION = 1440,
   CAPTURE_RESOLUTION = 512,
-  DENSITY_DISSIPATION = 3.5,
-  VELOCITY_DISSIPATION = 2,
+  DENSITY_DISSIPATION = 6.0,
+  VELOCITY_DISSIPATION = 4.0,
   PRESSURE = 0.1,
   PRESSURE_ITERATIONS = 20,
   CURL = 3,
-  SPLAT_RADIUS = 0.2,
-  SPLAT_FORCE = 6000,
+  SPLAT_RADIUS = 0.15,
+  SPLAT_FORCE = 3000,
   SHADING = true,
-  COLOR_UPDATE_SPEED = 10,
+  COLOR_UPDATE_SPEED = 5,
   BACK_COLOR = { r: 0.5, g: 0, b: 0 },
   TRANSPARENT = true,
 }) {
